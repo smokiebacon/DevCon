ddcd446150b4594678771aee6204949ad0fc10de
Abraham Hsu <smokiebacon@gmail.com>
2019-02-18 15:35:34 -0800

    adds profile body fields

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -35,6 +35,45 @@ router.get('/', passport.authenticate('jwt', {
     }
 );
 
+//GET /api/profile/ 
+// @desc CREATE user profile
+//@access is private
+router.post('/', passport.authenticate('jwt', {
+        session: false
+    }),
+    async (req, res) => {
+        const profileFields = {};
+        profileFields.user = req.user.id
+        if (req.body.handle) profileFields.handle = req.body.handle;
+        if (req.body.company) profileFields.company = req.body.company;
+        if (req.body.website) profileFields.website = req.body.website;
+        if (req.body.location) profileFields.location = req.body.location;
+        if (req.body.bio) profileFields.bio = req.body.bio;
+        if (req.body.status) profileFields.status = req.body.status;
+        if (req.body.githubusername)
+            profileFields.githubusername = req.body.githubusername;
+        //Skills, split into arrays b/c coming in as comma separated values
+        if (typeof req.body.skills !== 'undefined') {
+            profileFields.skills = req.body.skills.split(',');
+        }
+        // Social
+        profileFields.social = {};
+        if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+        if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+        if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+        if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+        if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+        try {
+
+
+        } catch {
+
+        }
+
+    }
+);
+
 
 
 module.exports = router;
