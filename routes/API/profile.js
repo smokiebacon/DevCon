c5bf84c3318a43340f9e2b730dcb724b2c6fa13e
Abraham Hsu <smokiebacon@gmail.com>
2019-02-18 15:20:42 -0800

    adds No Profile Found logic

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const Auth = require('../../models/Auth');
 
 //GET /api/profile/test
 // @desc tests auth route
@@ -7,6 +12,28 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: "Profile works"
 }));
+
+//GET /api/profile/ instead of :id, can get JWT token to uniquely identiy user
+// @desc get current user profile
+//@access is private
+router.get('/', passport.authenticate('jwt', {
+        session: false
+    }),
+    async (req, res) => {
+        const errors = {};
+        try {
+            const foundProfile = await Profile.findOne({
+                user: req.user.id
+            })
+            if (!foundProfile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+        } catch (err) {
+            res.status(404).json(err)
+        }
+    }
+);
 
 
 
