f0ac21173f0e2c2a01dd7c1d3ba65d8cb9988469
Abraham Hsu <smokiebacon@gmail.com>
2019-02-19 21:10:59 -0800

    adds post experience route

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -43,7 +43,7 @@ router.get(
 //GET /api/profile/all
 // @desc Get all profiles
 //@access is public
-router.get("/all", async (req, res) => {
+router.get('/all', async (req, res) => {
     const errors = {};
     try {
         const allUsers = await Profile.find({}).populate("user", ["name"]);
@@ -175,5 +175,53 @@ router.post(
         }
     }
 );
+
+
+//GET /api/profile/experience
+// @desc Add Experience to Profile
+//@access is Private
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post(
+    '/experience',
+    passport.authenticate('jwt', {
+        session: false
+    }),
+    (req, res) => {
+        const {
+            errors,
+            isValid
+        } = validateExperienceInput(req.body);
+
+        // Check Validation
+        if (!isValid) {
+            // Return any errors with 400 status
+            return res.status(400).json(errors);
+        }
+
+        Profile.findOne({
+            user: req.user.id
+        }).then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            };
+
+            // Add to exp array
+            profile.experience.unshift(newExp);
+
+            profile.save().then(profile => res.json(profile));
+        });
+    }
+);
+
+
+
 
 module.exports = router;
