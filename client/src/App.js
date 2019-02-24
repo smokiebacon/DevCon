314d9337598d09458632d7d23e2554e536b1dc2c
Abraham Hsu <smokiebacon@gmail.com>
2019-02-24 15:44:09 -0800

    adds React front-end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello World</h1>
+      <button className="btn btn-danger">delete
+      </button>
+      </div>
+    );
+  }
+}
+
+export default App;
