7842608494d18c66a77bd30fe90a719e19184865
Abraham Hsu <smokiebacon@gmail.com>
2019-02-24 18:54:30 -0800

    adds react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
       <Navbar />
       <Landing />
       <Footer />
-
       </div>
+      </Router>
     );
   }
 }
