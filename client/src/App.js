b07d241a967285270ed49a52dc684da5e6cecc61
Abraham Hsu <smokiebacon@gmail.com>
2019-02-24 16:01:55 -0800

    adds Navbar, Landing, and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Hello World</h1>
-      <button className="btn btn-danger">delete
-      </button>
+      <Navbar />
+      <Landing />
+      <Footer />
+
       </div>
     );
   }
