b9801a8806783604f6620fc776b835cfe899a6ed
Abraham Hsu <smokiebacon@gmail.com>
2019-03-07 20:25:55 -0800

    adds login redux functionality

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from '../utils/setAuthToken';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 //Register action
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/auth/register', userData)
@@ -16,7 +16,7 @@ export const registerUser = (userData, history) => dispatch => {
 
 //login get User token
 export const loginUser = userData => dispatch => {
-    axios.post('/api/users/login', userData)
+    axios.post('/api/auth/login', userData)
         .then(res => {
             //save to local storage
             const { token } = res.data;
@@ -26,6 +26,8 @@ export const loginUser = userData => dispatch => {
             setAuthToken(token);
             //decode jwt token to get user data
             const decoded = jwt_decode(token);
+            //set current user
+            dispatch(setCurrentUser(decoded));
         })
         .catch(err =>
         dispatch({
@@ -33,3 +35,11 @@ export const loginUser = userData => dispatch => {
             payload: err.response.data
         }) )
 };
+
+//set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
     state = {
         email: '',
         password: '',
         errors: {}
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard')
+
+        }
+        if(nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+
+            })
+        }
     }
     onChange = (e) => {
         this.setState({
@@ -14,9 +31,15 @@ class Login extends Component {
     }
     onSubmit = (e) => {
         e.preventDefault();
-        console.log('clicked login')
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        this.props.loginUser(userData)
     }
   render() {
+      const { errors } = this.state;
+
     return (
     <div className="login">
         <div className="container">
@@ -26,10 +49,24 @@ class Login extends Component {
             <p className="lead text-center">Sign in to your DevConnector account</p>
             <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+                <input type="email" 
+                className={classnames("form-control form-control-lg",
+                    {"is-invalid" : errors.email})}           
+                placeholder="Email Address" 
+                name="email" 
+                value={this.state.email} 
+                onChange={this.onChange} />
+                {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                 </div>
                 <div className="form-group">
-                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+                <input type="password" 
+                className={classnames("form-control form-control-lg",
+                    {"is-invalid" : errors.password})}                
+                placeholder="Password" 
+                name="password" 
+                value={this.state.password}
+                onChange={this.onChange}/>
+                {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
             </form>
@@ -40,4 +77,13 @@ class Login extends Component {
     )
   }
 }
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+export default connect(mapStateToProps, { loginUser })(Login);
