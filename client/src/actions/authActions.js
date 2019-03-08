853da4853fd96f78a9d8302b6a39aa50cf02492e
Abraham Hsu <smokiebacon@gmail.com>
2019-03-07 19:42:26 -0800

    adds jwt decoder

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types'
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import { GET_ERRORS } from './types';
 //Register action
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/auth/register', userData)
@@ -11,3 +13,23 @@ export const registerUser = (userData, history) => dispatch => {
         })
     )
 }
+
+//login get User token
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            //save to local storage
+            const { token } = res.data;
+            //set token to local storage
+            localStorage.setItem('jwtToken', token);
+            //set token to auth Header
+            setAuthToken(token);
+            //decode jwt token to get user data
+            const decoded = jwt_decode(token);
+        })
+        .catch(err =>
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }) )
+};
