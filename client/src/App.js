fa24e91d7ba4376a54ee03105b5916120ec6fe01
Abraham Hsu <smokiebacon@gmail.com>
2019-03-07 20:36:05 -0800

    adds global logged in, keep state upon refresh page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './Store';
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
@@ -11,6 +13,17 @@ import Login from './components/Auth/Login';
 
 import './App.css';
 
+//check for Token
+if(localStorage.jwtToken) {
+  //set auth Token header auth
+  setAuthToken(localStorage.token);
+  //decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Sets current user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+
+
+}
 class App extends Component {
   render() {
     return (
