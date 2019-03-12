783348c4603db2c3b06420a3105ed31e3a5ecd96
Abraham Hsu <smokiebacon@gmail.com>
2019-03-11 22:29:58 -0700

    Adds Dashboard.propTypes and mapStateToProps into Main.js

--- client/src/components/Dashboard/Main.js
+++ client/src/components/Dashboard/Main.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { getCurrentProfile } from '../../actions/profileActions'
+
+
+
 class Dashboard extends Component {
     componentDidMount () {
         this.props.getCurrentProfile();
@@ -15,4 +18,14 @@ class Dashboard extends Component {
   }
 }
 
-export default connect(null, { getCurrentProfile})(Dashboard);
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+})
+export default connect(mapStateToProps, { getCurrentProfile})(Dashboard);
