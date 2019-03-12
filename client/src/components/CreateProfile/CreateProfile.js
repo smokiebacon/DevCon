af2528e92774ead329edf226e695e51069899528
Abraham Hsu <smokiebacon@gmail.com>
2019-03-12 16:57:28 -0700

    renders TextFieldGroup in CreateProfile.js

--- client/src/components/CreateProfile/CreateProfile.js
+++ client/src/components/CreateProfile/CreateProfile.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextFieldFieldGroup from '../common/TextFieldGroup';
+import SelectListGroup from '../common/SelectListGroup';
+import InputGroup from '../common/InputGroup';
 
 class CreateProfile extends Component {
     state = {
@@ -21,7 +24,18 @@ class CreateProfile extends Component {
         instagram: '',
         errors: {}
     }
+    onChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        console.log('i am clicked')
+    }
   render() {
+      const { errors } = this.state;
     return (
     <div className="create-profile">
         <div className="container">
@@ -32,6 +46,17 @@ class CreateProfile extends Component {
                     Let's get some information to make your profile stand out!
                     </p>
                     <small className="d-block pb-3">* = required fields</small>
+                    <form onSubmit = { this.onSubmit }>
+                        <TextFieldFieldGroup 
+                            placeholder="* Profile Handle"
+                            name="handle"
+                            value={this.state.handle}
+                            onChange={this.onChange}
+                            error={errors.handle}
+                            info ="A unique handle for your profile URL. 
+                                Your full name, company name, and nickname."
+                        />
+                    </form>
                 </div>
             </div>
         </div>   
